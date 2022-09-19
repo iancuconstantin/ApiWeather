dad12282d4a732d053842386a4fdfb9349efcd3c
claudiumihait <claudium.tudor@gmail.com>
2022-09-19 20:33:19 +0300

    fetch city weather data by input

--- controller.js
+++ controller.js
@@ -1,4 +1,17 @@
 import * as model from './model.js' 
+import * as view from './view.js'
 
-model.getData()
-model.getSuggestedCities()
+const input = view.input
+
+input.addEventListener('keypress', (e)=> {
+    if (e.key === "Enter"){
+     const data = model.getCityWeatherData(e.target.value)
+     view.displayData(data)
+    }
+})
+
+
+
+export {
+    sendInput
+}
