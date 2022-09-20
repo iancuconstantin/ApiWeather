e7f28ee9d53406b918c8286dc4412b0eb1191c7d
claudiumihait <claudium.tudor@gmail.com>
2022-09-20 18:35:23 +0300

    wind + humidity, user interaction with wind, css

--- controller.js
+++ controller.js
@@ -4,6 +4,7 @@ import * as view from './view.js'
 const input = view.input;
 const temps = [view.tempC, view.tempF]
 const location = view.location
+const windIcon = view.windIcon
 
 let suggestionIndex;
 let suggestionsList = [];
@@ -12,72 +13,73 @@ input.addEventListener('keypress', async (e)=> {
   suggestionIndex = -1;
   if (e.key === "Enter"){
     try {
-		view.deleteSuggestions();
-		const imageList = await model.searchPhotos(e.target.value);
-		console.log(imageList);
-		const numberOfPages = Math.floor(imageList.total_results / 15);
-		const pageNumber = Math.floor(Math.random() * (numberOfPages + 1)) + 1;
-		const pageWithImages = await model.getPage(e.target.value, pageNumber);
-		const randomImageIndex = Math.floor(Math.random() * pageWithImages.photos.length);
-		const image = pageWithImages.photos[randomImageIndex];
-		console.log(image);
-		view.changeBackground(image);
+		// view.deleteSuggestions();
+		// const imageList = await model.searchPhotos(e.target.value);
+		// console.log(imageList);
+		// const numberOfPages = Math.floor(imageList.total_results / 15);
+		// const pageNumber = Math.floor(Math.random() * (numberOfPages + 1)) + 1;
+		// const pageWithImages = await model.getPage(e.target.value, pageNumber);
+		// const randomImageIndex = Math.floor(Math.random() * pageWithImages.photos.length);
+		// const image = pageWithImages.photos[randomImageIndex];
+		// console.log(image);
+		// view.changeBackground(image);
 		const data = await model.getCityWeatherData(e.target.value);
+		view.initCard()
 		view.displayData(data)
-		suggestionsList = [];
+		// suggestionsList = [];
     } catch (error) {
       	console.log(error);
     }
   }
 })
 
-input.addEventListener('input', async (e) => {
-  view.deleteSuggestions();
- try {
-	 if (e.target.value.length > 2) {
-	    const data = await model.searchCities(e.target.value);
-	    console.log(e.target.value);
-	    suggestionsList = view.displaySuggestions(data);
-	    for (const element of suggestionsList) {
-	      element.addEventListener('click', async (e) => {
-	        const data = await model.getCityWeatherData(element.innerText)
-	        view.displayData(data);
-	        view.deleteSuggestions();
-	        view.inputBarAutocomplete(element.innerText);
-	      });
-	    }
-	  } 
-  } catch (error) {
-	  console.log(error)
-  }
-})
+// input.addEventListener('input', async (e) => {
+//   view.deleteSuggestions();
+//  try {
+// 	 if (e.target.value.length > 2) {
+// 	    const data = await model.searchCities(e.target.value);
+// 	    console.log(e.target.value);
+// 	    suggestionsList = view.displaySuggestions(data);
+// 	    for (const element of suggestionsList) {
+// 	      element.addEventListener('click', async (e) => {
+// 	        const data = await model.getCityWeatherData(element.innerText)
+// 	        view.displayData(data);
+// 	        view.deleteSuggestions();
+// 	        view.inputBarAutocomplete(element.innerText);
+// 	      });
+// 	    }
+// 	  } 
+//   } catch (error) {
+// 	  console.log(error)
+//   }
+// })
 
-document.addEventListener('keydown', (e) => {
-  let length = suggestionsList.length;
-  if (length > 0) {
-    if(e.code === 'ArrowDown') {
-		suggestionIndex++;
-		if (suggestionsList[suggestionIndex - 1]) {
-			suggestionsList[suggestionIndex - 1].classList.remove('div-selected');
-		}
-		if (suggestionIndex === length) {
-			suggestionIndex = 0;
-		}
-		suggestionsList[suggestionIndex].classList.add('div-selected');
-		view.inputBarAutocomplete(suggestionsList[suggestionIndex].innerText);
-		} else if (e.code === 'ArrowUp') {
-		suggestionIndex--;
-		if (suggestionsList[suggestionIndex + 1]) {
-			suggestionsList[suggestionIndex + 1].classList.remove('div-selected');
-		}
-		if (suggestionIndex === -1 || suggestionIndex === -2) {
-			suggestionIndex = length - 1;
-		}
-		suggestionsList[suggestionIndex].classList.add('div-selected');
-		view.inputBarAutocomplete(suggestionsList[suggestionIndex].innerText);
-    }
-  }
-});
+// document.addEventListener('keydown', (e) => {
+//   let length = suggestionsList.length;
+//   if (length > 0) {
+//     if(e.code === 'ArrowDown') {
+// 		suggestionIndex++;
+// 		if (suggestionsList[suggestionIndex - 1]) {
+// 			suggestionsList[suggestionIndex - 1].classList.remove('div-selected');
+// 		}
+// 		if (suggestionIndex === length) {
+// 			suggestionIndex = 0;
+// 		}
+// 		suggestionsList[suggestionIndex].classList.add('div-selected');
+// 		view.inputBarAutocomplete(suggestionsList[suggestionIndex].innerText);
+// 		} else if (e.code === 'ArrowUp') {
+// 		suggestionIndex--;
+// 		if (suggestionsList[suggestionIndex + 1]) {
+// 			suggestionsList[suggestionIndex + 1].classList.remove('div-selected');
+// 		}
+// 		if (suggestionIndex === -1 || suggestionIndex === -2) {
+// 			suggestionIndex = length - 1;
+// 		}
+// 		suggestionsList[suggestionIndex].classList.add('div-selected');
+// 		view.inputBarAutocomplete(suggestionsList[suggestionIndex].innerText);
+//     }
+//   }
+// });
 
 //handle display of temp by selection
 temps.forEach(temp => {
@@ -85,5 +87,10 @@ temps.forEach(temp => {
         const data = await model.getCityWeatherData(location.innerText)
         view.getTempBySelection(data, temp)
     })
+})
+
+windIcon.addEventListener('click', async ()=>{
+	const data = await model.getCityWeatherData(location.innerText)
+	view.convertWind(data.current.wind_mph, data.current.wind_kph)
 })
 
